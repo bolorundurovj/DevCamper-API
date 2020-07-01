0c91bbf05d5a80107c5a9c0953b5398670db1577
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-07-01 12:11:55 +0100

    Home route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Hello from Express');
+})
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
