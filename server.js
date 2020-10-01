9059f709ad7d5decf324123c860263e75dbc44a9
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-01 11:54:24 +0100

    Basic Route Setup

--- server.js
+++ server.js
@@ -6,8 +6,20 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express');
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'Show All Bootcamps'})
+})
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success: true, msg: 'Create New Bootcamp'})
+})
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Diaplay Bootcamp ${req.params.id}`})
+})
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Update Bootcamp ${req.params.id}`})
+})
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Delete Bootcamp ${req.params.id}`})
 })
 
 const PORT = process.env.PORT || 5000;
