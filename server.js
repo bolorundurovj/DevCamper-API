ba425f70548f8753281137f6e2480f77ca5b96bc
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-03 15:36:54 +0100

    DB Connect

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+
+//Load Env Vars
+dotenv.config({ path: './config/config.env' });
+
+//Connect to database
+connectDB();
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
 //Middleware Imports
 const logger = require('./middleware/logger');
-
-//Load Env Vars
-dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
@@ -30,6 +35,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
+    `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`.yellow
   )
 );
