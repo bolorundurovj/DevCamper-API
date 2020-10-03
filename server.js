6f9161f7f4513207c915880cae5d499f925e6189
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-03 11:32:40 +0100

    Added Morgan

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require("express");
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
 const colors = require('colors');
+const morgan = require('morgan');
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
@@ -9,15 +10,26 @@ const bootcamps = require('./routes/bootcamps');
 const logger = require('./middleware/logger');
 
 //Load Env Vars
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+//My logging middleware
 app.use(logger);
 
+//Dev logging middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 //Mount routers
-app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`));
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
+  )
+);
