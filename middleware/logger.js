cf94bfe5c1c570a3d7a66632a8b7ec89be9eeb4c
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-08 22:04:23 +0100

    ch: Get Response Status in logger

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,7 @@
 // @desc    Logs request to console
 const logger = (req, res, next) => {
     req.hello = 'Hello World';
-    console.info(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`.bold.blue.bgWhite);
+    console.info(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl} `.bold.blue, `${res.statusCode}`.bold.green);
     next();
 }
 
