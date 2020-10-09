96d809e714b733620668a97e9cdb7fc55efd8e7d
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-09 13:51:47 +0100

    ft: Mongoose Middleware - Slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -112,11 +113,11 @@ const BootcampSchema = new mongoose.Schema({
     // 
   );
   
-//   // Create bootcamp slug from the name
-//   BootcampSchema.pre('save', function(next) {
-//     this.slug = slugify(this.name, { lower: true });
-//     next();
-//   });
+  // Create bootcamp slug from the name
+  BootcampSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+  });
   
 //   // Geocode & create location field
 //   BootcampSchema.pre('save', async function(next) {
