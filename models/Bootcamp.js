43554bb7366af504b6b7bc3179a5b16dd0304da9
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-09 15:02:06 +0100

    ft: Geocode location

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -39,25 +40,23 @@ const BootcampSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Please add an address']
       },
-      // location: {
-      //   // GeoJSON Point
-      //   type: {
-      //     type: String,
-      //     enum: ['Point'],
-      //     required: true
-      //   },
-      //   coordinates: {
-      //     type: [Number],
-      //     index: '2dsphere',
-      //     required: true
-      //   },
-      //   formattedAddress: String,
-      //   street: String,
-      //   city: String,
-      //   state: String,
-      //   zipcode: String,
-      //   country: String
-      // },
+      location: {
+        // GeoJSON Point
+        type: {
+          type: String,
+          enum: ['Point']
+        },
+        coordinates: {
+          type: [Number],
+          index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String
+      },
       careers: {
         // Array of strings
         type: [String],
@@ -101,16 +100,7 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
       },
-    //   user: {
-    //     type: mongoose.Schema.ObjectId,
-    //     ref: 'User',
-    //     required: true
-    //   }
     },
-    // {
-    //   toJSON: { virtuals: true },
-    //   toObject: { virtuals: true }
-    // 
   );
   
   // Create bootcamp slug from the name
@@ -119,24 +109,24 @@ const BootcampSchema = new mongoose.Schema({
     next();
   });
   
-//   // Geocode & create location field
-//   BootcampSchema.pre('save', async function(next) {
-//     const loc = await geocoder.geocode(this.address);
-//     this.location = {
-//       type: 'Point',
-//       coordinates: [loc[0].longitude, loc[0].latitude],
-//       formattedAddress: loc[0].formattedAddress,
-//       street: loc[0].streetName,
-//       city: loc[0].city,
-//       state: loc[0].stateCode,
-//       zipcode: loc[0].zipcode,
-//       country: loc[0].countryCode
-//     };
+  // Geocode & create location field
+  BootcampSchema.pre('save', async function(next) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+      type: 'Point',
+      coordinates: [loc[0].longitude, loc[0].latitude],
+      formattedAddress: loc[0].formattedAddress,
+      street: loc[0].streetName,
+      city: loc[0].city,
+      state: loc[0].stateCode,
+      zipcode: loc[0].zipcode,
+      country: loc[0].countryCode
+    };
   
-//     // Do not save address in DB
-//     this.address = undefined;
-//     next();
-//   });
+    // // Do not save address in DB
+    // this.address = undefined;
+    next();
+  });
   
 //   // Cascade delete courses when a bootcamp is deleted
 //   BootcampSchema.pre('remove', async function(next) {
