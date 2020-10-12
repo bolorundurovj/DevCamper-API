a00bf5b61965869d94fb16fadce3f4394ada82a3
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-12 20:35:06 +0100

    ft: Add Single Course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,9 +1,11 @@
+const Bootcamp = require('../models/Bootcamp');
 const Course = require('../models/Course');
 const colors = require('colors');
 const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
 
 // @desc    Get all courses
+// @route   GET /api/v1/courses
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
 
@@ -15,13 +17,45 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   } else {
     query = Course.find().populate({
       path: 'bootcamp',
-      select: 'name description'
+      select: 'name description',
     });
   }
 
   const courses = await query;
 
-  res
-    .status(200)
-    .json({ success: true, count: courses.length, data: courses });
+  res.status(200).json({ success: true, count: courses.length, data: courses });
+});
+
+// @desc    Get a single course
+// @route   GET /api/v1/courses/:id
+// @access  Public
+
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await (await Course.findById(req.params.id)).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!course) {
+    return next(new ErrorResponse(`No course with the id of ${req.params.id}`, 404))
+  }
+
+  res.status(200).json({ success: true, data: course });
+});
+
+// @desc    Add a course
+// @route   POST /api/v1/bootcamps/:bootcampId/courses
+// @access  Private
+
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+  const bootcamp = await (await Bootcamp.findById(req.params.bootcampId));
+
+  if (!bootcamp) {
+    return next(new ErrorResponse(`No bootcamp with the id of ${req.params.bootcampId}`, 404))
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(200).json({ success: true, data: course });
 });
