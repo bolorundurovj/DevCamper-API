139326a4cd9244808836d9940465a9d6733aa206
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-13 08:01:16 +0100

    ch: Added Middleware for file upload

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
-const connectDB = require('./config/db');
+const fileupload = require('express-fileupload');
 
+const connectDB = require('./config/db');
 
 //Load Env Vars
 dotenv.config({ path: './config/config.env' });
@@ -25,12 +26,15 @@ const app = express();
 app.use(logger);
 
 //Body Parser
-app.use(express.json())
+app.use(express.json());
 
 //Dev logging middleware
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
+
+//File Uploading
+app.use(fileupload());
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
@@ -43,7 +47,8 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`.yellow.bold
+    `Server running in ${process.env.NODE_ENV} mode on port http://localhost:${PORT}`
+      .yellow.bold
   )
 );
 
@@ -51,5 +56,7 @@ const server = app.listen(
 process.on('unhandledRejection', (err, promise) => {
   console.log(`Error: ${err.message}`.red.bgWhite);
   //Close server && exit process
-  server.close(() => {process.exit(1)})
-})
+  server.close(() => {
+    process.exit(1);
+  });
+});
