1b990b7e7e741eda1e3f121fc7d1f30860e8ff7a
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-18 12:29:22 +0100

    Register User

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ connectDB();
 //Route files
 const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
+const auth = require('./routes/auth');
 
 //Middleware Imports
 const logger = require('./middleware/logger');
@@ -43,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/auth', auth);
 
 app.use(errorHandler);
 
