2b1f523e60a3cc7565154897a307dca488e20405
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-18 16:39:59 +0100

    Role Authorization

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,21 +13,21 @@ const Bootcamp = require('../models/Bootcamp');
 const advancedResults = require('../middleware/advancedResults');
 
 const courseRouter = require('./courses');
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 const router = express.Router();
 
 router.route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(protect, createBootcamp);
+  .post(protect, authorize('publisher', 'admin'), createBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(protect, updateBootcamp)
-  .delete(protect, deleteBootcamp);
+  .put(protect, authorize('publisher', 'admin'), updateBootcamp)
+  .delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
   router.use('/:bootcampId/courses', courseRouter);
   router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-  router.route('/:id/photo').put(protect, bootcampPhotoUpload);
+  router.route('/:id/photo').put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
 module.exports = router;
