274ccb238c7346090e032edfb7319227ea0f078b
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-18 16:13:40 +0100

    Auth Middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -13,20 +13,21 @@ const Bootcamp = require('../models/Bootcamp');
 const advancedResults = require('../middleware/advancedResults');
 
 const courseRouter = require('./courses');
+const { protect } = require('../middleware/auth');
 const router = express.Router();
 
 router.route('/')
   .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
-  .post(createBootcamp);
+  .post(protect, createBootcamp);
 
 router
   .route('/:id')
   .get(getBootcamp)
-  .put(updateBootcamp)
-  .delete(deleteBootcamp);
+  .put(protect, updateBootcamp)
+  .delete(protect, deleteBootcamp);
 
   router.use('/:bootcampId/courses', courseRouter);
   router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
-  router.route('/:id/photo').put(bootcampPhotoUpload);
+  router.route('/:id/photo').put(protect, bootcampPhotoUpload);
 
 module.exports = router;
