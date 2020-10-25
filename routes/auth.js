27544199a76d237184253573c611093a63fcdd33
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-25 17:39:48 +0100

    Update User Details

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
+const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.put('/updatedetails', protect, updateDetails);
+router.post('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
 router.put('/forgotpassword/:resettoken', resetPassword);
 router.get('/me', protect, getMe);
