48658978cf93d6806f46c825aadebeaa2bd49b28
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-25 14:33:24 +0100

    Generate Forgot-Password Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,10 +48,31 @@ exports.login = asyncHandler(async (req, res, next) => {
 });
 
 // @desc      Get current logged in user
-// @route     POST /api/v1/auth/me
+// @route     GET /api/v1/auth/me
 // @access    Private
 exports.getMe = asyncHandler(async (req, res, next) => {
   const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// @desc      Forgot Password
+// @route     POST /api/v1/auth/forgotpassword
+// @access    Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  //Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
 
   res.status(200).json({
     success: true,
